5b019b56bfeaea226ee1354fa06610df347b6b86
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-05 21:15:21 +0100

    Delete config-section.tsx

--- src/app/components/configBar/config-section.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {
-  children: React.ReactNode;
-};
-
-export function ConfigSection({ children }: Props) {
-  return <section className="flex gap-4">{children}</section>;
-}
