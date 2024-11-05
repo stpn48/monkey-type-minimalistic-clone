dc4486abf99535b6e334f42f54005730feaa5611
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-05 21:15:19 +0100

    Create quote-size-options.tsx

---
+++ src/app/components/config-bar/quote-size-options.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ConfigButton } from "@/app/components/config-bar/config-button";
+import { ConfigSection } from "@/app/components/config-bar/config-section";
+import { QuoteLength, useConfigState } from "@/context/useConfigState";
+
+export function QuotesOptions() {
+  const { quoteLength, setQuoteLength } = useConfigState();
+  const quoteLengths: QuoteLength[] = ["short", "medium", "long"];
+
+  return (
+    <ConfigSection>
+      {quoteLengths.map((length) => (
+        <ConfigButton key={length} active={length === quoteLength} onClick={() => setQuoteLength(length)}>
+          {length}
+        </ConfigButton>
+      ))}
+    </ConfigSection>
+  );
+}
