b3207523a0ef5f2c8bdf9b9f010e2d40a6d6a3a7
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-06 18:58:40 +0100

    Create parsers.ts

---
+++ src/nuqs/parsers.ts
@@ -0,0 +1,26 @@
+import { Mode, QuoteLength } from "@/context/useConfigState";
+import { createParser } from "nuqs";
+
+export const modeParser = createParser<Mode>({
+  parse(queryValue) {
+    if (queryValue === "time" || queryValue === "wordCount" || queryValue === "quote") {
+      return queryValue as Mode; // Valid values
+    }
+    return null; // Invalid value
+  },
+  serialize(value) {
+    return value; // Directly return the mode value
+  },
+});
+
+export const quoteLengthParser = createParser<QuoteLength>({
+  parse(queryValue) {
+    if (queryValue === "short" || queryValue === "medium" || queryValue === "long") {
+      return queryValue as QuoteLength; // Valid values
+    }
+    return null; // Invalid value
+  },
+  serialize(value) {
+    return value; // Directly return the mode value
+  },
+});
