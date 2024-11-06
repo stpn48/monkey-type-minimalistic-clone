4a53c4b54e934f8c74d82e1a26783fe4f32fe7d2
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-06 07:30:00 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import { Logo } from "@/components/icons";
+import { Footer } from "@/app/components/footer";
+import { Header } from "@/app/components/header";
+import { MainContent } from "@/app/components/main-content";
 import { ToggleThemeButton } from "@/components/toggle-theme-button";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-background">
-      <Logo />
+    <div className="flex h-screen flex-col px-10 py-4">
+      <Header />
+      <MainContent />
+      <Footer />
       <ToggleThemeButton />
     </div>
   );
