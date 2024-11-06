e84170954ae6e7cbfde7babac792ec3a1a99647a
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-06 18:58:38 +0100

    Update word.tsx

--- src/app/components/typing-field/word.tsx
+++ src/app/components/typing-field/word.tsx
@@ -1,58 +1,25 @@
 import { Letter } from "@/app/components/typing-field/letter";
-import { useTypingField } from "@/context/useTypingField";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 type Props = {
   word: string;
-  wordIndex: number;
+  userWord: string;
 };
 
 const MemoLetter = React.memo(Letter);
 
-export function Word({ word, wordIndex }: Props) {
-  const { activeWordIndex, setActiveWordIndex, activeLetterIndex, setActiveLetterIndex } = useTypingField();
-  const [userWord, setUserWord] = useState("");
-
-  function handleKeyDown(e: KeyboardEvent) {
-    if (e.code === "Space") {
-      // handle space click
-      if (word.length <= activeLetterIndex) {
-        setActiveWordIndex((prev) => prev + 1);
-        setActiveLetterIndex(0);
-      }
-    }
-
-    if (e.code === "Backspace") {
-      setActiveLetterIndex((prev) => prev - 1);
-    }
-
-    // handle other key presses
-    setUserWord((prev) => prev + e.key.toLowerCase());
-  }
-
-  useEffect(() => {
-    if (wordIndex === activeWordIndex) {
-      window.addEventListener("keydown", handleKeyDown);
-
-      return () => {
-        window.removeEventListener("keydown", handleKeyDown);
-      };
-    }
-  }, [wordIndex, activeWordIndex, handleKeyDown]);
-
+export function Word({ word, userWord }: Props) {
   return (
     <div>
-      {Array.from(word).map((char, letterIndex) => (
-        <MemoLetter key={letterIndex} char={char} isCorrect={validateIsCorrect(userWord[letterIndex], char)} />
-      ))}
+      {Array.from(word).map((letter, letterIndex) => {
+        return (
+          <MemoLetter key={letterIndex} letter={letter} userLetter={userWord[letterIndex] || ""} />
+        );
+      })}
+      {userWord.length > word.length &&
+        Array.from(userWord.slice(word.length, word.length + 20)).map((letter, letterIndex) => {
+          return <MemoLetter key={letterIndex} letter={letter} userLetter={"overflow-letter"} />;
+        })}
     </div>
   );
-}
-
-function validateIsCorrect(letter: string | undefined, correctLetter: string) {
-  if (letter === undefined) {
-    return null;
-  }
-
-  return letter === correctLetter;
 }
