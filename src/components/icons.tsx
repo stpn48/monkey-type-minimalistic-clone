e3a7fd1d91ad527552447ebfee0967e73efd0735
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-06 07:30:01 +0100

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,9 +1,9 @@
 export function Logo() {
   return (
     <svg
-      width="50"
-      height="50"
-      className="fill-text"
+      width="42"
+      height="42"
+      className="fill-primary"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       viewBox="-680 -1030 300 180"
