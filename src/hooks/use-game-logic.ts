ef14e158451a12f8de4c5e996e479f193ff13726
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-08 21:53:58 +0100

    Update use-game-logic.ts

--- src/hooks/use-game-logic.ts
+++ src/hooks/use-game-logic.ts
@@ -1,26 +1,12 @@
 import { useConfigState } from "@/context/useConfigState";
-import { useRouter } from "next/navigation";
-import { useCallback, useEffect } from "react";
 import { useKeyHandlers } from "./use-key-handlers";
 import { useModeHandler } from "./use-mode-handler";
-import { useTimer } from "./use-timer";
 
 export function useGameLogic(words: string[]) {
-  const router = useRouter();
-
-  const handleFinishGame = useCallback(() => {
-    router.push("/you-win"); //TODO: Implement
-  }, [router]);
-
   // all the game settings
-  const { mode, wordCount, timeDuration } = useConfigState();
-  // timer logic
-  const { startTimer, time, timeoutId } = useTimer(timeDuration, handleFinishGame);
+  const { mode, wordCount } = useConfigState();
   // handle key presses
-  useKeyHandlers(mode, timeoutId, startTimer, words);
-
+  useKeyHandlers(mode, words);
   // handle wordCount mode and quote mode
-  useModeHandler({ mode, words, handleFinishGame, wordCount });
-
-  return { time };
+  useModeHandler({ mode, words, wordCount });
 }
