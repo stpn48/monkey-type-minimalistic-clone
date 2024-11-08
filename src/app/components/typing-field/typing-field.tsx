c7df339de09d4e1972ab1fa8b2f2d12b29de4e70
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-08 21:53:56 +0100

    Update typing-field.tsx

--- src/app/components/typing-field/typing-field.tsx
+++ src/app/components/typing-field/typing-field.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Caret } from "@/app/components/typing-field/caret";
+import { Timer } from "@/app/components/typing-field/timer";
 import { Word } from "@/app/components/typing-field/word";
 import { useTypingField } from "@/context/use-typing-field";
 import { useConfigState } from "@/context/useConfigState";
@@ -8,21 +10,32 @@ import { useGenerateWords } from "@/hooks/use-generate-words";
 import React from "react";
 
 const MemoWord = React.memo(Word);
+const MemoTimer = React.memo(Timer);
 
 export function TypingField() {
   const { words } = useGenerateWords();
   const { userWords } = useTypingField();
-  const { mode } = useConfigState();
-
-  const { time } = useGameLogic(words);
+  const { mode, timeDuration } = useConfigState();
+  useGameLogic(words);
 
   return (
     <>
-      {mode === "time" && <p>{time}</p>}
-      <div className="flex flex-wrap gap-4 px-20 font-geist-mono">
-        {words.map((word, wordIndex) => (
-          <MemoWord key={wordIndex} word={word} userWord={userWords[wordIndex] || ""} />
-        ))}
+      <div className="flex flex-col gap-10 px-20 font-geist-mono">
+        {mode === "time" && (
+          <MemoTimer startTimer={!!userWords[0][0]} timeDuration={timeDuration} />
+        )}
+
+        <div className="flex flex-wrap gap-4">
+          <Caret />
+          {words.map((word, wordIndex) => (
+            <MemoWord
+              key={wordIndex}
+              word={word}
+              userWord={userWords[wordIndex] || ""}
+              wordIndex={wordIndex}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
