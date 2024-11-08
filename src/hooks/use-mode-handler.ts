f9bc13110bc12ea0e1593266827f99681082c9e4
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-08 21:54:00 +0100

    Update use-mode-handler.ts

--- src/hooks/use-mode-handler.ts
+++ src/hooks/use-mode-handler.ts
@@ -1,21 +1,23 @@
 import { useTypingField } from "@/context/use-typing-field";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 type Args = {
   words: string[];
   mode: string;
-  handleFinishGame: () => void;
   wordCount: number;
 };
 
-export function useModeHandler({ words, mode, handleFinishGame, wordCount }: Args) {
+export function useModeHandler({ words, mode, wordCount }: Args) {
   const { activeLetterIndex, activeWordIndex } = useTypingField();
+
+  const router = useRouter();
 
   useEffect(() => {
     if (mode === "wordCount") {
       if (activeWordIndex === wordCount - 1 && activeLetterIndex == words[activeWordIndex].length) {
-        handleFinishGame();
+        router.push("/results");
       }
     }
-  }, [activeWordIndex, wordCount, activeLetterIndex, words, mode, handleFinishGame]);
+  }, [activeWordIndex, wordCount, activeLetterIndex, words, mode, router]);
 }
