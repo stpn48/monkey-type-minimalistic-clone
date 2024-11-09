91c25a2db41a34eb46dec9f1d8739b18697961f4
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-09 17:34:26 +0100

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,12 +1,24 @@
+"use client";
+
 import { Logo } from "@/components/icons";
+import { useTypingField } from "@/context/use-typing-field";
+import { motion } from "framer-motion";
 import { Settings, UserRound } from "lucide-react";
+import { twMerge } from "tailwind-merge";
 
 export function Header() {
+  const { userTyping } = useTypingField();
+
   return (
-    <header className="flex w-full px-20">
-      <div className="flex items-center gap-3">
+    <header
+      className={twMerge(
+        "opacity-1 flex w-full px-20 transition-opacity duration-300",
+        userTyping && "opacity-0",
+      )}
+    >
+      <div className="flex items-center gap-2">
         <Logo />
-        <h1 className="text-2xl font-bold text-logoText">Type</h1>
+        <h1 className="text-text-primary font-geist-sans text-2xl font-bold">tpye</h1>
       </div>
 
       <div className="flex-1" />
