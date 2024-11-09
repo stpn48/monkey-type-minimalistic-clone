363d3f3781d710c052927e470f5357c5012b4d43
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-09 17:34:25 +0100

    Update config-bar.tsx

--- src/app/components/config-bar/config-bar.tsx
+++ src/app/components/config-bar/config-bar.tsx
@@ -6,13 +6,21 @@ import { ModeSection } from "@/app/components/config-bar/mode-section";
 import { QuotesOptions } from "@/app/components/config-bar/quote-size-options";
 import { TimeOptions } from "@/app/components/config-bar/time-options";
 import { WordsOptions } from "@/app/components/config-bar/word-count-options";
+import { useTypingField } from "@/context/use-typing-field";
 import { useConfigState } from "@/context/useConfigState";
+import { twMerge } from "tailwind-merge";
 
 export function ConfigBar() {
   const { mode } = useConfigState();
+  const { userTyping } = useTypingField();
 
   return (
-    <div className="flex w-full justify-center">
+    <div
+      className={twMerge(
+        "flex w-full justify-center transition-opacity duration-300",
+        userTyping && "opacity-0",
+      )}
+    >
       <div className="flex items-center gap-6 rounded-lg bg-foreground px-4 py-2 shadow-md transition-all">
         {mode !== "quote" && <IncludeSection />}
 
