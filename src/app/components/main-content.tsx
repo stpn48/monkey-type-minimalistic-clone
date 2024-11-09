82e63e66c7cef3d552dfee0895f15e771377eb66
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-09 11:27:53 +0100

    Update main-content.tsx

--- src/app/components/main-content.tsx
+++ src/app/components/main-content.tsx
@@ -1,10 +1,18 @@
 import { TypingField } from "@/app/components/typing-field/typing-field";
+import { CapsLockAlert } from "./caps-lock-alert";
 import { ConfigBar } from "./config-bar/config-bar";
+import { Timer } from "./typing-field/timer";
 
 export function MainContent() {
   return (
-    <main className="flex h-full w-full flex-col gap-16">
+    <main className="flex flex-col px-20">
       <ConfigBar />
+
+      <div className="relative flex h-[100px] w-full items-center">
+        <Timer />
+        <CapsLockAlert />
+      </div>
+
       <TypingField />
     </main>
   );
