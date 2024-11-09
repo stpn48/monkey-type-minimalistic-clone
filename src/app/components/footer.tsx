9cae03b6e47f9abd7dd52b9accb514feb319f08e
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-09 17:34:25 +0100

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,9 +1,20 @@
+"use client";
+
 import { ToggleThemeButton } from "@/components/toggle-theme-button";
+import { useTypingField } from "@/context/use-typing-field";
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 export function Footer() {
+  const { userTyping } = useTypingField();
+
   return (
-    <div className="fixed bottom-8 flex w-screen items-center justify-between px-8">
+    <div
+      className={twMerge(
+        "fixed bottom-8 flex w-screen items-center justify-between px-8 transition-opacity duration-300",
+        userTyping && "opacity-0",
+      )}
+    >
       <h1>footer</h1>
       <ToggleThemeButton />
     </div>
