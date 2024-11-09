59032365eaaf3c1ad1c85d56977c773316f1af88
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-09 22:41:25 +0100

    Create page.tsx

---
+++ src/app/results/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+export default function ResultsPage({}: Props) {
+  return (
+    <div className="p-4">
+      <h1>WPM</h1>
+      <h1>ACCURACY</h1>
+      <h1>WPM</h1>
+    </div>
+  );
+}
