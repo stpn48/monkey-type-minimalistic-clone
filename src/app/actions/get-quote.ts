deef4565e12a30b5499f09486f6ca132627996b0
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-09 11:27:51 +0100

    Create get-quote.ts

---
+++ src/app/actions/get-quote.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { QuoteLength } from "@/context/useConfigState";
+import prisma from "@/utils/prisma";
+
+export async function getQuote(length: QuoteLength) {
+  try {
+    const quotes = await prisma.quote.findMany({
+      where: {
+        length: length,
+      },
+    });
+
+    // choose random quote
+    console.log("Quotes successfully fetched");
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+  } catch (error) {
+    console.error(error);
+  }
+}
