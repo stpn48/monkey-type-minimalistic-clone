b483452cd8a7014996634b64714282cd9acf9047
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-09 22:41:24 +0100

    Update word.tsx

--- src/app/components/typing-field/word.tsx
+++ src/app/components/typing-field/word.tsx
@@ -5,13 +5,18 @@ type Props = {
   word: string;
   userWord: string;
   wordIndex: number;
+  isBehind: boolean;
 };
 
 const MemoLetter = React.memo(Letter);
 
-export function Word({ word, userWord, wordIndex }: Props) {
+export function Word({ word, userWord, wordIndex, isBehind }: Props) {
+  const hasMistake = isBehind && userWord !== word;
+
   return (
-    <div className={`word-${wordIndex}`}>
+    <div
+      className={`word-${wordIndex} ${hasMistake && "underline decoration-incorrect-text decoration-2 underline-offset-4"}`}
+    >
       {Array.from(word).map((letter, letterIndex) => {
         return (
           <MemoLetter key={letterIndex} letter={letter} userLetter={userWord[letterIndex] || ""} />
