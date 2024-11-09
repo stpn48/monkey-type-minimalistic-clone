376bec2ce5ea46eed278b7bdb3f0cbb31083875a
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-09 11:27:53 +0100

    Update caret.tsx

--- src/app/components/typing-field/caret.tsx
+++ src/app/components/typing-field/caret.tsx
@@ -1,17 +1,23 @@
 "use client";
 
+import { useTypingField } from "@/context/use-typing-field";
 import { useCaretPosition } from "@/hooks/useCaretPosition";
 import { useRef } from "react";
+import { twMerge } from "tailwind-merge";
 
 export function Caret({ ...props }: React.HTMLAttributes<HTMLSpanElement>) {
   const caretRef = useRef<HTMLSpanElement>(null);
   useCaretPosition(caretRef);
+  const { userTyping } = useTypingField();
 
   return (
     <span
       ref={caretRef}
       {...props}
-      className="absolute h-[36px] w-[3px] rounded-full bg-primary transition-all duration-[165ms]"
+      className={twMerge(
+        "absolute h-[36px] w-[3px] rounded-full bg-primary transition-opacity duration-[300ms] ease-in-out",
+        !userTyping && "animate-caretFlashSmooth",
+      )}
     />
   );
 }
