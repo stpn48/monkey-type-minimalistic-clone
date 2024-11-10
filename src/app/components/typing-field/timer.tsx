1258c6e5ccd4c715630012cca6720e95d8c31105
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-10 17:23:35 +0100

    Update timer.tsx

--- src/app/components/typing-field/timer.tsx
+++ src/app/components/typing-field/timer.tsx
@@ -2,13 +2,14 @@
 
 import { useTypingField } from "@/context/use-typing-field";
 import { useConfigState } from "@/context/useConfigState";
+import { formatTime } from "@/utils/format-time";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 let intervalId: NodeJS.Timeout | null = null;
 
 export default function Timer() {
-  const { startTimer } = useTypingField();
+  const { startTimer, setFinishedTypingTime } = useTypingField();
   const { timeDuration } = useConfigState();
 
   const [time, setTime] = useState(timeDuration);
@@ -18,17 +19,7 @@ export default function Timer() {
   useEffect(() => {
     if (startTimer) {
       intervalId = setInterval(() => {
-        setTime((prev) => {
-          // if time is up, redirect to results page
-          if (prev < 1) {
-            clearInterval(intervalId!);
-            intervalId = null;
-            router.push("/results");
-            return 0;
-          }
-
-          return prev - 1;
-        });
+        setTime((prev) => prev - 1);
       }, 1000);
 
       return () => {
@@ -38,18 +29,16 @@ export default function Timer() {
   }, [startTimer]);
 
   useEffect(() => {
+    if (time === 0) {
+      clearInterval(intervalId!);
+      setFinishedTypingTime(new Date().getTime());
+      router.push("/results");
+    }
+  }, [time, router, intervalId]);
+
+  useEffect(() => {
     setTime(timeDuration);
   }, [timeDuration]);
 
   return <p className="font-geist-mono text-3xl text-primary">{formatTime(time)}</p>;
-}
-
-function formatTime(timeSeconds: number) {
-  const minutes = Math.floor(timeSeconds / 60);
-  const seconds = timeSeconds % 60;
-
-  if (minutes === 0) {
-    return `${seconds < 10 ? "0" : ""}${seconds}`;
-  }
-  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
