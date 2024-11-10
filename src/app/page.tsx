e7dcb8c6f4fc67632e94eda5db7214435cfe6892
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-10 17:23:37 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { Footer } from "@/app/components/footer";
-import { Header } from "@/app/components/header";
 import { MainContent } from "./components/main-content";
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-screen flex-col gap-10 py-8">
-      <Header />
+    <div className="flex w-screen flex-col gap-10">
       <MainContent />
       <Footer />
     </div>
