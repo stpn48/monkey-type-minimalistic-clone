6832e0c1afb7373b031a4c0cfbad2b5d8dac8d5d
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-10 17:23:43 +0100

    Create init-infinite-words-handler.ts

---
+++ src/hooks/init-infinite-words-handler.ts
@@ -0,0 +1,46 @@
+import { useTypingField } from "@/context/use-typing-field";
+import { generate as generateRandomWords } from "random-words";
+import { useCallback, useEffect } from "react";
+
+export function initInfiniteWordsHandler(
+  includeNumbers: boolean,
+  generateWords: (wordCount?: number, includeNumbers?: boolean) => string[],
+) {
+  const {
+    currRow,
+    firstRowWordCount,
+    setFirstRowWordCount,
+    activeWordIndex,
+    setUserWords,
+    setActiveWordIndex,
+    setCurrRow,
+    setWords,
+  } = useTypingField();
+
+  const handleThirdRow = useCallback(() => {
+    // if on row 3 generate new words, append new words and remove the first row
+    const newWords = generateWords(firstRowWordCount, includeNumbers);
+
+    setWords((prev) => [...prev.slice(firstRowWordCount), ...newWords]);
+    setUserWords((prev) => [...prev.slice(firstRowWordCount)]);
+    setFirstRowWordCount(0);
+    setActiveWordIndex((prev) => prev - firstRowWordCount);
+    setCurrRow((prev) => prev - 1);
+  }, [includeNumbers, firstRowWordCount, setWords, setUserWords]);
+
+  const handleInfiniteWords = useCallback(() => {
+    if (currRow === 2 && firstRowWordCount === 0) {
+      console.log("first row words", activeWordIndex);
+      setFirstRowWordCount(activeWordIndex);
+      return;
+    }
+
+    if (currRow === 3) {
+      handleThirdRow();
+    }
+  }, [handleThirdRow, firstRowWordCount, currRow]);
+
+  useEffect(() => {
+    handleInfiniteWords();
+  }, [handleInfiniteWords]);
+}
