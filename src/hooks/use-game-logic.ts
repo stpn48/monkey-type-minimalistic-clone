818a8de8e9468d8d79c6aca22b52416bd8030e3e
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-11 21:24:31 +0100

    Update use-game-logic.ts

--- src/hooks/use-game-logic.ts
+++ src/hooks/use-game-logic.ts
@@ -1,12 +1,7 @@
-import { useConfigState } from "@/context/useConfigState";
 import { useKeyHandlers } from "./use-key-handlers";
 import { useModeHandler } from "./use-mode-handler";
 
 export function useGameLogic() {
-  // all the game settings
-  const { mode } = useConfigState();
-  // handle key presses
-  useKeyHandlers(mode);
-  // handle wordCount mode and quote mode
-  useModeHandler(mode);
+  useKeyHandlers();
+  useModeHandler();
 }
