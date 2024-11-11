04698ccce460321a0786b0880127882325d08ff3
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-11 07:21:08 +0100

    Update letter.tsx

--- src/app/components/typing-field/letter.tsx
+++ src/app/components/typing-field/letter.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useStatisticsStore } from "@/context/use-statistics";
+import { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 
 type Props = {
@@ -6,12 +10,22 @@ type Props = {
 };
 
 export function Letter({ letter, userLetter }: Props) {
+  const { setMistakes } = useStatisticsStore();
+
+  const isCorrect = userLetter === letter;
+
+  useEffect(() => {
+    if (!isCorrect && userLetter !== "" && userLetter !== "overflow-letter") {
+      setMistakes((prev) => prev + 1);
+    }
+  }, [isCorrect, userLetter, setMistakes]);
+
   return (
     <span
       className={twMerge(
         "font-geist-mono text-3xl",
-        userLetter === letter && "text-text-primary",
-        userLetter !== letter && "text-incorrect-text",
+        isCorrect && "text-text-primary",
+        !isCorrect && "text-incorrect-text",
         userLetter === "" && "text-text",
         userLetter === "overflow-letter" && "text-incorrect-text-overflow",
       )}
