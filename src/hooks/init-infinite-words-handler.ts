697a87869f25fc1c8be87bd0ac70536fd66ce286
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-11 21:24:30 +0100

    Update init-infinite-words-handler.ts

--- src/hooks/init-infinite-words-handler.ts
+++ src/hooks/init-infinite-words-handler.ts
@@ -1,6 +1,7 @@
-import { useStatisticsStore } from "@/context/use-statistics";
 import { useTypingField } from "@/context/use-typing-field";
-import { useCallback, useEffect } from "react";
+import { useConfigState } from "@/context/useConfigState";
+import { set } from "lodash";
+import { useCallback, useEffect, useState } from "react";
 
 export function initInfiniteWordsHandler(
   includeNumbers: boolean,
@@ -15,20 +16,45 @@ export function initInfiniteWordsHandler(
     setActiveWordIndex,
     setCurrRow,
     setWords,
+    words,
+    totalWordsGenerated,
+    setTotalWordsGenerated,
   } = useTypingField();
 
-  const { totalWords } = useStatisticsStore();
+  const { wordCount } = useConfigState();
 
   const handleThirdRow = useCallback(() => {
-    // if on row 3 generate new words, append new words and remove the first row
-    const newWords = generateWords(firstRowWordCount, includeNumbers);
+    // if on row 3 generate new words, append new words and remove the first ro
+
+    const wordsLeftToGenerate = wordCount - totalWordsGenerated;
+    console.log("words left to generate", wordsLeftToGenerate);
+    let newWords: string[] = [];
+
+    if (wordsLeftToGenerate === 0) {
+      return;
+    }
+
+    if (wordsLeftToGenerate >= firstRowWordCount) {
+      newWords = generateWords(wordsLeftToGenerate, includeNumbers);
+      setTotalWordsGenerated((prev) => prev + newWords.length);
+    } else {
+      newWords = generateWords(wordsLeftToGenerate, includeNumbers);
+    }
 
     setWords((prev) => [...prev.slice(firstRowWordCount), ...newWords]);
     setUserWords((prev) => [...prev.slice(firstRowWordCount)]);
     setFirstRowWordCount(0);
     setActiveWordIndex((prev) => prev - firstRowWordCount);
     setCurrRow((prev) => prev - 1);
-  }, [includeNumbers, firstRowWordCount, setWords, setUserWords]);
+  }, [
+    includeNumbers,
+    firstRowWordCount,
+    setWords,
+    setUserWords,
+    wordCount,
+    words,
+    totalWordsGenerated,
+  ]);
 
   const handleInfiniteWords = useCallback(() => {
     if (currRow === 2 && firstRowWordCount === 0) {
@@ -40,7 +66,7 @@ export function initInfiniteWordsHandler(
     if (currRow === 3) {
       handleThirdRow();
     }
-  }, [handleThirdRow, firstRowWordCount, currRow]);
+  }, [handleThirdRow, firstRowWordCount, currRow, activeWordIndex]);
 
   useEffect(() => {
     handleInfiniteWords();
