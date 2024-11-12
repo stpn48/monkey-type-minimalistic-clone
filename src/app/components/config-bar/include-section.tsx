7913891196bee157cbc184948c5abf990891e3b0
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-12 19:48:51 +0100

    Update include-section.tsx

--- src/app/components/config-bar/include-section.tsx
+++ src/app/components/config-bar/include-section.tsx
@@ -1,20 +1,27 @@
 "use client";
 
-import { useConfigState } from "@/context/useConfigState";
+import { useConfigState } from "@/context/use-config-state";
 import { ConfigButton } from "./config-button";
 import { ConfigSection } from "./config-section";
 import { ConfigSpacer } from "./config-spacer";
 
 export function IncludeSection() {
-  const { includeNumbers, setIncludeNumbers, includePunctuation, setIncludePunctuation } = useConfigState();
+  const { includeNumbers, setIncludeNumbers, includePunctuation, setIncludePunctuation } =
+    useConfigState();
 
   return (
     <>
       <ConfigSection>
-        <ConfigButton active={includePunctuation === true} onClick={() => setIncludePunctuation((prev) => !prev)}>
+        <ConfigButton
+          active={includePunctuation === true}
+          onClick={() => setIncludePunctuation((prev) => !prev)}
+        >
           @ punctuation
         </ConfigButton>
-        <ConfigButton active={includeNumbers === true} onClick={() => setIncludeNumbers((prev) => !prev)}>
+        <ConfigButton
+          active={includeNumbers === true}
+          onClick={() => setIncludeNumbers((prev) => !prev)}
+        >
           # numbers
         </ConfigButton>
       </ConfigSection>
