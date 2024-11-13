404348dd3d1259d209e6640422b5fb6a1c1648ec
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-13 18:21:45 +0100

    Update register-form.tsx

--- src/app/login/components/register-form.tsx
+++ src/app/login/components/register-form.tsx
@@ -3,18 +3,41 @@
 import { signUp } from "@/app/actions/supabase-auth/sign-up";
 import { Button } from "@/components/button";
 import { Input } from "@/components/input";
-import React from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState, useTransition } from "react";
 import { toast } from "react-hot-toast";
+import { useEmailValidation } from "../hooks/use-email-valiation";
+import { usePasswordValidation } from "../hooks/use-password-validation";
+import { useUsernameValidation } from "../hooks/use-username-validation";
 
 export function RegisterForm() {
-  const [isRegistering, startRegistering] = React.useTransition();
+  const [isRegistering, startRegistering] = useTransition();
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+  const router = useRouter();
+
+  const {
+    passwordInputPopupMsg,
+    isPasswordValid,
+    handlePasswordChange,
+    confirmPasswordInputPopupMsg,
+    isConfirmPasswordValid,
+    handleConfirmPasswordChange,
+  } = usePasswordValidation();
+
+  const { handleUsernameChange, usernameInputPopupMsg, isUsernameValid, validatingUsername } =
+    useUsernameValidation();
+
+  const { handleEmailChange, emailInputPopupMsg, isEmailValid } = useEmailValidation();
 
   const handleRegisterFormSubmit = (formData: FormData) => {
-    const username = formData.get("username");
+    const username = formData.get("username") as string;
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
     const confirmPassword = formData.get("confirmPassword");
 
+    // additional checks. This should never happen but just in case
     if (!username || !email || !password || !confirmPassword) {
       toast.error("Please fill out all fields");
       return;
@@ -26,20 +49,66 @@ export function RegisterForm() {
     }
 
     startRegistering(async () => {
-      signUp(email, password);
+      const { error, data } = await signUp(username, email, password);
 
-      // TODO: init userData
+      if (error) {
+        toast.error(error.message);
+        return;
+      }
       toast.success("Registration successful");
+      router.push("/account");
     });
   };
 
+  useEffect(() => {
+    setIsButtonDisabled(!(isEmailValid && isPasswordValid && isUsernameValid));
+  }, [isEmailValid, isPasswordValid, isUsernameValid]);
+
   return (
-    <form action={handleRegisterFormSubmit} className="flex flex-col gap-2">
-      <Input placeholder="username" name="username" />
-      <Input placeholder="email" type="email" name="email" />
-      <Input placeholder="password" type="password" name="password" />
-      <Input placeholder="confirm password" type="password" name="confirmPassword" />
-      <Button type="submit">Register</Button>
+    <form action={handleRegisterFormSubmit} className="flex w-fit flex-col gap-2">
+      <Input
+        disabled={isRegistering}
+        isLoading={validatingUsername}
+        placeholder="username"
+        onChange={handleUsernameChange}
+        showValidation
+        isContentValid={isUsernameValid}
+        popupMsg={usernameInputPopupMsg}
+        name="username"
+      />
+      <Input
+        disabled={isRegistering}
+        showValidation
+        placeholder="email"
+        onChange={handleEmailChange}
+        type="email"
+        name="email"
+        isContentValid={isEmailValid}
+        popupMsg={emailInputPopupMsg}
+      />
+      <Input
+        disabled={isRegistering}
+        showValidation
+        isContentValid={isPasswordValid}
+        popupMsg={passwordInputPopupMsg}
+        placeholder="password"
+        type="password"
+        name="password"
+        onChange={handlePasswordChange}
+      />
+      <Input
+        disabled={isRegistering}
+        showValidation
+        isContentValid={isConfirmPasswordValid}
+        popupMsg={confirmPasswordInputPopupMsg}
+        placeholder="confirm password"
+        type="password"
+        name="confirmPassword"
+        onChange={handleConfirmPasswordChange}
+      />
+      <Button className="w-full" disabled={isButtonDisabled || isRegistering} type="submit">
+        Register
+      </Button>
     </form>
   );
 }
