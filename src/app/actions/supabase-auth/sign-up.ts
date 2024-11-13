0e8d72d25aa0a321b7b24831394d91be68928396
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-13 18:21:41 +0100

    Update sign-up.ts

--- src/app/actions/supabase-auth/sign-up.ts
+++ src/app/actions/supabase-auth/sign-up.ts
@@ -1,7 +1,8 @@
 "use server";
 
+import prisma from "@/utils/prisma";
 import { createClient } from "@/utils/supabase/server";
-import { Session, User, WeakPassword } from "@supabase/supabase-js";
+import { Session, User } from "@supabase/supabase-js";
 
 type SignUpResponse = {
   error: null | Error;
@@ -11,7 +12,11 @@ type SignUpResponse = {
   };
 };
 
-export async function signUp(email: string, password: string): Promise<SignUpResponse> {
+export async function signUp(
+  username: string,
+  email: string,
+  password: string,
+): Promise<SignUpResponse> {
   // TODO: check if the received arguments are valid
 
   const supabase = await createClient();
@@ -22,6 +27,26 @@ export async function signUp(email: string, password: string): Promise<SignUpRes
   });
 
   if (error) return { error, data: null };
+
+  await prisma.userData.create({
+    data: {
+      username,
+      stats: {
+        create: {
+          allTimeWpm: 0,
+          totalTestsCompleted: 0,
+          totalTestsStarted: 0,
+          totalTimeTyped: 0,
+          totalWordsTyped: 0,
+          totalLettersTyped: 0,
+          totalCorrectLettersTyped: 0,
+          totalMistakes: 0,
+        },
+      },
+    },
+  });
+
+  await supabase.auth.signInWithPassword({ email, password });
 
   return { error: null, data };
 }
