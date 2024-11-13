9b08e455cc4683c464fa3280c23ea25a83274665
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-13 21:54:00 +0100

    Delete page.tsx

--- src/app/account/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { getUserData } from "../actions/get-user-data";
-
-type Props = {};
-
-export default async function AccountPage({}: Props) {
-  const { data, error } = await getUserData();
-  return <div className="">{data?.username || "no user logged in"}</div>;
-}
