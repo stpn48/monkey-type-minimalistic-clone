5d0135170df19109d7476817a2a6b0f687c9568d
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-14 22:56:55 +0100

    Update prisma.ts

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,17 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  const globalWithPrisma = global as typeof globalThis & {
-    prisma: PrismaClient;
-  };
-  if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient();
-  }
-  prisma = globalWithPrisma.prisma;
-}
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 export default prisma;
