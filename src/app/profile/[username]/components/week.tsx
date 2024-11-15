750fe052596f6db4142f96cc3d2b3b7d8450e481
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-15 12:18:45 +0100

    Create week.tsx

---
+++ src/app/profile/[username]/components/week.tsx
@@ -0,0 +1,25 @@
+import { Activity } from "@prisma/client";
+import { Day } from "./day";
+
+type Props = {
+  week: {
+    date: Date;
+  }[];
+
+  activities: Activity[];
+};
+
+export function Week({ week, activities }: Props) {
+  return (
+    <div className="flex flex-col gap-2">
+      {week.map((day, dayIndex) => {
+        // find the activity for this day
+        const activityThisDay = activities.find(
+          (activity) => activity.createdAt.getTime() === day.date.getTime(),
+        );
+
+        return <Day key={dayIndex} dayDate={day.date} activityThisDay={activityThisDay} />;
+      })}
+    </div>
+  );
+}
