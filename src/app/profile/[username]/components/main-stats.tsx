fc50a14aa3152c5c688b0f06b588822a667f3335
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-15 12:18:45 +0100

    Update main-stats.tsx

--- src/app/profile/[username]/components/main-stats.tsx
+++ src/app/profile/[username]/components/main-stats.tsx
@@ -8,23 +8,29 @@ type Props = {
     totalTestsCompleted: number;
     totalTestsStarted: number;
     totalTimeTyped: number;
+    totalWordsTyped: number;
+    totalLettersTyped: number;
+    totalCorrectLettersTyped: number;
+    totalMistakes: number;
   };
 };
 
 export function MainInfo({ username, stats }: Props) {
   return (
     <section className="flex w-[400px] flex-col gap-10 rounded-lg bg-foreground p-8">
-      <h1 className="text-4xl text-text-primary">{username}</h1>
-
-      <div className="flex flex-col gap-4">
-        <Statistic label="ALL TIME WPM" value={stats.allTimeWpm.toString()} />
+      <h1 className="text-3xl text-text-primary">{username}</h1>
+      <div className="grid grid-cols-2 gap-x-20 gap-y-10">
+        <Statistic label="ALL TIME WPM" value={stats.allTimeWpm.toFixed(2).toString()} />
         <Statistic label="TOTAL TESTS COMPLETED" value={stats.totalTestsCompleted.toString()} />
         <Statistic label="TOTAL TESTS STARTED" value={stats.totalTestsStarted.toString()} />
         <Statistic label="TOTAL TIME TYPED" value={stats.totalTimeTyped.toString()} />
-        <Statistic label="TOTAL WORDS TYPED" value={stats.totalTimeTyped.toString()} />
-        <Statistic label="TOTAL LETTERS TYPED" value={stats.totalTimeTyped.toString()} />
-        <Statistic label="TOTAL CORRECT LETTERS TYPED" value={stats.totalTimeTyped.toString()} />
-        <Statistic label="TOTAL MISTAKES" value={stats.totalTimeTyped.toString()} />
+        <Statistic label="TOTAL WORDS TYPED" value={stats.totalWordsTyped.toString()} />
+        <Statistic label="TOTAL LETTERS TYPED" value={stats.totalLettersTyped.toString()} />
+        <Statistic
+          label="TOTAL CORRECT LETTERS TYPED"
+          value={stats.totalCorrectLettersTyped.toString()}
+        />
+        <Statistic label="TOTAL MISTAKES" value={stats.totalMistakes.toString()} />
       </div>
     </section>
   );
