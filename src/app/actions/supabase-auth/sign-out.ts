583c5d9a919fb8cd6c4c11c009895e085eefc044
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-16 10:02:05 +0100

    Update sign-out.ts

--- src/app/actions/supabase-auth/sign-out.ts
+++ src/app/actions/supabase-auth/sign-out.ts
@@ -1,9 +1,14 @@
 "use server";
 
 import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function signOut() {
   const supabase = await createClient();
 
   await supabase.auth.signOut();
+
+  revalidatePath("/");
+  redirect("/login");
 }
