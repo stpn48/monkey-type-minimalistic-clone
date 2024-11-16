dd8f1db10a6a96f60b1ae306e8ccb29f7cf04ca5
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-16 10:28:58 +0100

    Create toggle-toast-msg-button.tsx

---
+++ src/app/components/toggle-toast-msg-button.tsx
@@ -0,0 +1,23 @@
+// "use client";
+
+// import React from "react";
+// import toast from "react-hot-toast";
+
+// export function ToggleToastMsgButton() {
+//   return (
+//     <div className="flex flex-col gap-4">
+//       <button
+//         className="rounded-lg bg-foreground p-4"
+//         onClick={() => toast.success("Success toast message")}
+//       >
+//         Show success toast
+//       </button>
+//       <button
+//         className="rounded-lg bg-foreground p-4"
+//         onClick={() => toast.error("Error toast message")}
+//       >
+//         Show error toast
+//       </button>
+//     </div>
+//   );
+// }
