350c27735b200d0bf17b2f8a02273e3d8cc34a01
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-16 10:02:11 +0100

    Update format-time.ts

--- src/utils/format-time.ts
+++ src/utils/format-time.ts
@@ -1,9 +1,10 @@
 export function formatTime(timeSeconds: number) {
+  const hours = Math.floor(timeSeconds / 3600);
   const minutes = Math.floor(timeSeconds / 60);
   const seconds = timeSeconds % 60;
 
   if (minutes === 0) {
     return `${seconds < 10 ? "0" : ""}${seconds}`;
   }
-  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  return `${hours !== 0 ? `${hours < 10 ? "0" : ""}${hours}:` : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
