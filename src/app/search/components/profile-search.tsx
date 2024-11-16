174edc61cb44c49161a83034b26c39f9f6270643
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-16 21:48:04 +0100

    Update profile-search.tsx

--- src/app/search/components/profile-search.tsx
+++ src/app/search/components/profile-search.tsx
@@ -2,7 +2,7 @@
 
 import { getSuggestions } from "@/app/actions/get-suggestions";
 import { Input } from "@/components/input";
-import { LoaderPinwheel } from "lucide-react";
+import { LoaderPinwheel, Search } from "lucide-react";
 import Link from "next/link";
 import { useQueryState } from "nuqs";
 import { useEffect, useState, useTransition } from "react";
@@ -34,19 +34,24 @@ export function ProfileSearch({}: Props) {
   }, [query]);
 
   return (
-    <div className="relative flex flex-col gap-4">
-      <form className="flex gap-2">
+    <div className="relative flex flex-col">
+      <form className="flex">
         <Input
+          className={`${query ? "rounded-b-none rounded-t-lg rounded-tr-none" : "rounded-lg rounded-r-none"} w-full`}
           defaultValue={query || ""}
           placeholder="Search for a username"
           onChange={(e) => setQuery(e.target.value)}
         />
-        <button>SEARCH</button>
+        <button
+          className={`rounded-tr-lg bg-foreground px-4 ${query ? "rounded-tr-lg" : "rounded-r-lg"}`}
+        >
+          <Search className="size-5" />
+        </button>
       </form>
 
       {/* search suggestions */}
       {query && (
-        <div className="absolute top-full mt-2 flex w-full flex-col gap-2 rounded-lg bg-foreground p-4">
+        <div className="absolute top-full z-10 flex w-full flex-col gap-2 rounded-b-lg bg-foreground p-4">
           {isLoading && (
             <div className="flex w-full justify-center">
               <LoaderPinwheel className="animate-spin" />
