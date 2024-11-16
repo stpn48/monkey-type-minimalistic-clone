42f388dcca319713033dde4bd7328e1b3500a4a5
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-16 10:02:32 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Footer } from "@/app/components/footer";
 import { MainContent } from "./components/main-content";
 
 export default function Home() {
   return (
     <div className="mt-10 flex">
       <MainContent />
-      <Footer />
     </div>
   );
 }
