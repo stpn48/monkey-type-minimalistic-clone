d8ff060d37800478d068b07c74f49a5b5cc962c5
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-16 21:48:02 +0100

    Update main-stats.tsx

--- src/app/profile/[username]/components/main-stats.tsx
+++ src/app/profile/[username]/components/main-stats.tsx
@@ -21,7 +21,7 @@ export function MainInfo({ username, stats }: Props) {
     <section className="flex flex-col gap-10 rounded-lg bg-foreground p-10">
       <h1 className="text-3xl text-text-primary">{username}</h1>
       <div className="grid grid-cols-1 gap-x-20 gap-y-10 sm:grid-cols-2 lg:grid-cols-4">
-        <Statistic label="ALL TIME WPM" value={stats.allTimeWpm.toFixed(2).toString()} />
+        <Statistic label="15 SEC ALL TIME WPM" value={stats.allTimeWpm.toFixed(2).toString()} />
         <Statistic label="TOTAL TESTS COMPLETED" value={stats.totalTestsCompleted.toString()} />
         <Statistic label="TOTAL TESTS STARTED" value={stats.totalTestsStarted.toString()} />
         <Statistic label="TOTAL TIME TYPED" value={formatTime(stats.totalTimeTyped).toString()} />
