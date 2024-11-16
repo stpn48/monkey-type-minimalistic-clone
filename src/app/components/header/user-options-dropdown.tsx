ef6ba3bbaa163ac768720bb37d4f177d94e91a42
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-16 21:47:59 +0100

    Update user-options-dropdown.tsx

--- src/app/components/header/user-options-dropdown.tsx
+++ src/app/components/header/user-options-dropdown.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 export function UserOptionsDropdown() {
   return (
-    <div className="invisible absolute top-full flex flex-col gap-2 whitespace-nowrap rounded-lg bg-foreground p-2 text-text shadow-md group-hover:visible">
-      <Link href={"/login"} className="rounded-md px-4 py-1 hover:bg-background">
-        Your Profile
-      </Link>
-      <SignOutButton />
+    <div className="invisible absolute top-full whitespace-nowrap bg-transparent pt-2 group-hover:visible">
+      <div className="flex flex-col gap-2 rounded-lg bg-foreground p-2 text-text shadow-md">
+        <Link href={"/login"} className="rounded-md px-4 py-1 hover:bg-background">
+          Your Profile
+        </Link>
+        <SignOutButton />
+      </div>
     </div>
   );
 }
