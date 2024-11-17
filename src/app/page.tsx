7bb565b481ed3d0dc3cec1e866a99b6c5949814c
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-17 15:17:29 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Loader2 } from "lucide-react";
 import { Suspense } from "react";
 import { MainContent } from "./components/main-content";
 
 export default function Home() {
   return (
-    <Suspense fallback={<Loader2 />}>
+    <Suspense fallback={<></>}>
       <div className="mt-10 flex">
         <MainContent />
       </div>
