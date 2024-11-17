dda977fc983983a806657c53a86954e39df74b6a
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-17 15:04:35 +0100

    Update search-button.tsx

--- src/app/components/header/search-button.tsx
+++ src/app/components/header/search-button.tsx
@@ -4,9 +4,7 @@ import { Search } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-type Props = {};
-
-export function SearchButton({}: Props) {
+export function SearchButton() {
   return (
     <Link href={"/search"} className="group">
       <Search className="size-5 group-hover:text-primary" />
