cc26aafa485455c978dd4e2bd51a4bb73fb06657
stepan <133258815+stpn48@users.noreply.github.com>
2024-11-17 15:04:35 +0100

    Update header.tsx

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,4 +1,5 @@
-import { Search, Settings } from "lucide-react";
+import { Loader2, Settings } from "lucide-react";
+import { Suspense } from "react";
 import { HeaderContainer } from "./header-container";
 import { LogoButton } from "./logo-button";
 import { SearchButton } from "./search-button";
@@ -6,22 +7,24 @@ import { UserButton } from "./user-button";
 
 export function Header() {
   return (
-    <HeaderContainer>
-      <LogoButton />
+    <Suspense fallback={<Loader2 />}>
+      <HeaderContainer>
+        <LogoButton />
 
-      <div className="flex-1" />
+        <div className="flex-1" />
 
-      <ul className="flex items-center gap-4">
-        <li>
-          <UserButton />
-        </li>
-        <li>
-          <SearchButton />
-        </li>
-        <li>
-          <Settings className="size-5" />
-        </li>
-      </ul>
-    </HeaderContainer>
+        <ul className="flex items-center gap-4">
+          <li>
+            <UserButton />
+          </li>
+          <li>
+            <SearchButton />
+          </li>
+          <li>
+            <Settings className="size-5" />
+          </li>
+        </ul>
+      </HeaderContainer>
+    </Suspense>
   );
 }
